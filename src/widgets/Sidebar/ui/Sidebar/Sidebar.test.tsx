26b33806cccf80612db76541ecee441d9e92202e
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-03-02 09:05:57 +0300

    Added routing for test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from './Sidebar';
-import { RenderWithTranslation } from 'shared/lib/helpers/tests/renderWithTranslation/RenderWithTranslation';
+import { ComponentRender } from 'shared/lib/helpers/tests/componentRender/ComponentRender';
 
 describe('Sidebar', () => {
   test('checking for sidebar', () => {
-    RenderWithTranslation(<Sidebar />);
+    ComponentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('checking for sidebar', () => {
-    RenderWithTranslation(<Sidebar />);
+    ComponentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     fireEvent.click(toggleBtn);
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
