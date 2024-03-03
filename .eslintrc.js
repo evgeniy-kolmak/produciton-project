c653fc82f4850dee8af7dfa7ba206cd3ebc766aa
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-03-03 13:07:53 +0300

    Added react-hooks plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,15 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:i18next/recommended', 'prettier', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:i18next/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
   overrides: [
     {
       env: {
@@ -21,7 +29,13 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react', 'i18next', 'prettier'],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'i18next',
+    'prettier',
+  ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
@@ -29,6 +43,8 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'i18next/no-literal-string': 2,
     'prettier/prettier': 'warn',
   },
