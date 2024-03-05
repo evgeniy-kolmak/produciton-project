89fcbc1db0fe4f2c241959d120cb6ac56e8ef5ee
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-03-05 17:37:59 +0300

    Removed ThemeProvider

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -6,19 +6,11 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import { App } from 'app/App';
-import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'widgets/ErrorBoundary';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      element={
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      }
-      errorElement={<ErrorBoundary />}
-    >
+    <Route element={<App />} errorElement={<ErrorBoundary />}>
       {Object.values(routeConfig).map(({ path, element }) => (
         <Route key={path} path={path} element={element} />
       ))}
